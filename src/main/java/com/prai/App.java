0b3450dde91353378793edf193c393bccd44d771
epraria <prashanna.rai@ericsson.com>
2023-05-07 16:44:27 +0200

    corrected source code

--- src/main/java/com/prai/App.java
+++ src/main/java/com/prai/App.java
@@ -1,55 +1,32 @@
 package com.prai;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Map;
-import by.borge.jarl.Jarl;
 import java.util.List;
 
 /**
- * Hello world!
+ * Testing with Jarl library!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        String uri="files/upload/updateservice/package";
-        String method = "POST";
-        List<String> roles=Arrays.asList("OmcEquipmentObserver",
-                "CreateJob",
+
+        System.out.println( "******************************************" );
+        String uri1="Chassis";
+        String method1 = "GET";
+        List<String> roles1=Arrays.asList("OmcEquipmentAdministrator");
+       boolean localDecision= OPADecisionMaker.isAllowedJarl(uri1,method1,roles1);
+        List<String> roles=Arrays.asList("CreateJob",
                 "DeleteJob",
                 "OmcEquipmentAdministrator");
-        boolean opadecision=OPADecisionMaker.isAllowed(uri,method,roles);
-        if (opadecision) {
-            System.out.println( "OPA Decision Allowed" );
-        }
-//        System.out.println( "******************************************" );
 
-        List<String> methods  =Arrays.asList("GET","POST","GET","GET","POST","GET");
-       List<String> uris=Arrays.asList("/Managers/my-pod/Oem/Ericsson_2/RemoteBackupService/Actions/Ericsson2RemoteBackupService.CreateAndTransferBackup",
-               "/TaskService/Tasks/1/",
-               "/Managers/prai-pod/Oem/Ericsson_2/RemoteBackupService/Actions/Ericsson2RemoteBackupService.CreateAndTransferBackup",
-               "/Managers/vanja-pod/Oem/Ericsson_2/RemoteBackupService/Actions/Ericsson2RemoteBackupService.CreateAndTransferBackup",
-               "files/upload/updateservice/package",
-               "/TaskService/Tasks/3/");
-//        String method1 = "GET";
-//
-//        List<String> allowedUris=OPADecisionMaker.isAllowed(uris,method1,roles);
-//        System.out.println( "******************************************" );
-//        System.out.println( "" );
-//
-//
-//        allowedUris.forEach(System.out::println);
-        System.out.println( "******************************************" );
-
-       boolean localDecision= OPADecisionMaker.isAllowedJarl(uri,method,roles);
+        List<String> uris=Arrays.asList("/TaskService/Tasks/1/",
+                "files/upload/updateservice/package",
+                "/TaskService/Tasks/3/",
+                "Chassis");
+        List<String> methods=Arrays.asList("GET","GET","GET","GET");
+        List<String> allowedUris=OPADecisionMaker.isAllowedJarl(uris,methods,roles);
         System.out.println( "The value of decision from Jarl " );
-        System.out.println( localDecision );
-
-       // OPADecisionMaker.isAllowedJarl(uris,method,roles);
-        OPADecisionMaker.isAllowed(uris,methods,roles);
-        System.out.println("****");
     }
 }
--- src/main/java/com/prai/OPADecisionMaker.java
+++ src/main/java/com/prai/OPADecisionMaker.java
@@ -1,5 +1,6 @@
 package com.prai;
 
+import by.borge.jarl.Jarl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -10,24 +11,69 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.*;
-
-import by.borge.jarl.Jarl;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OPADecisionMaker {
-//private static String serverIP= "testserver";
-//    private static String serverPort= "32323";
+    private static String serverIP= "testserver";
+    private static String serverPort= "32323";
+    private static String coarsegrainedendpoint = "http://"+serverIP+":"+serverPort+"/v1/data/authz/redfish/v1/policy";
+    private static String finegrainedendpoint = "http://"+serverIP+":"+serverPort+"/v1/data/authz/redfish/v1/fine/policy";
 
-    private static final String serverIP= "testserver";
-    private static final String serverPort= "32323";
-    private static final String coarsegrainedendpoint = "http://"+serverIP+":"+serverPort+"/v1/data/authz/redfish/v1/policy";
-    private static final String finegrainedendpoint = "http://"+serverIP+":"+serverPort+"/v1/data/authz/redfish/v1/fine/policy";
+    private static String coarsedGrainedIRFile= "/repo/policy/optimizedPlans/coarse-grained-policies-plan.json";
+    private static String coarsedGrainedentrypoint= "authz/redfish/v1/policy/allow";
+    private static String fineGrainedIRFile= "/repo/policy/optimizedPlans/fine-grained-policies-plan.json";
+    private static String fineGrainedIRFileentrypoint="authz/redfish/v1/fine/policy/batch_allow";
+    public static boolean isAllowedJarl(String uri, String method, List<String> roles) {
+        var file = new File(coarsedGrainedIRFile);
+        boolean decision = false;
 
-    private static final String IRFile= "/repo/policy/optimized/authz/redfish/v1/plan.json";
-    private static final String entrypoint= "authz/redfish/v1/policy/allow";
-    private static final String IRFile2= "/repo/policy/policies/optimized/authz/redfish/v1/fine/plan.json";
+        Map<String, ?> data = Map.of();
+        Map<String,Object> map = new HashMap<>();
+        map.put("roles", roles);
+        map.put("method",method);
+        map.put("resource",uri);
 
-    private static final String entrypoint2="authz/redfish/v1/fine/policy/batch_allow";
+        try {
+            decision= Jarl.builder(file)
+                    .build()
+                    .getPlan(coarsedGrainedentrypoint)
+                    .eval(map, data).allowed();
+        } catch (IOException e) {
+            System.out.println( "Exception while making decision by Jarl library " +e.getStackTrace() );
+        }
+        finally{
+            return decision;
+        }
+    }
+
+
+    public static List<String> isAllowedJarl(List < String > uris, List < String > methods, List < String > roles) {
+      //  String IRFile2= "/repo/policy/optimizedPlans/fine-grained-policies-plan.json";
+        var file = new File(fineGrainedIRFile);
+        List<String> allowedUrls= Collections.EMPTY_LIST;
+        Map<String, ?> data = Map.of();
+        Map<String,Object> map = new HashMap<>();
+        map.put("roles", roles);
+        map.put("methods",methods);
+        map.put("resources",uris);
+        try {
+            var resultSet = Jarl.builder(file)
+                    .build()
+                    .getPlan(fineGrainedIRFileentrypoint)
+                    .eval(map, data);
+            allowedUrls= (List<String>) resultSet.getFirst().getValue(List.class).stream().map(Object::toString).collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println( "Exception while making decision by Jarl library " +e.getStackTrace() );
+        }
+        finally{
+            return allowedUrls;
+        }
+    }
+
     public static boolean isAllowed(String uri, String method, List<String> roles) {
         boolean opaDecision;
         ObjectMapper objectMapper = new ObjectMapper();
@@ -43,8 +89,9 @@ public class OPADecisionMaker {
         SingleOPAResponse response;
         try {
             String inputJson = objectMapper.writeValueAsString(opaRequest);
-            System.out.println("OPADecisionMaker InputJSON created .....  ");
-            System.out.print(inputJson);
+
+        //    LOGGER.info("OPADecisionMaker InputJSON created .....{}", inputJson);
+
             HttpRequest httpRequest = HttpRequest.newBuilder()
                     .POST(HttpRequest.BodyPublishers.ofString(inputJson))
                     .uri(URI.create(coarsegrainedendpoint))
@@ -53,17 +100,15 @@ public class OPADecisionMaker {
 
             HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-            System.out.println("OPADecisionMaker httpResponse .....  ");
-            System.out.print(httpResponse);
-
             response = objectMapper.readValue(httpResponse.body(), SingleOPAResponse.class);
-            System.out.println("OPADecisionMaker httpResponse Body....");
-            System.out.print(httpResponse.body());
+
+        //    LOGGER.info("OPADecisionMaker httpResponse Body....{}", httpResponse.body());
 
             opaDecision = response.getResult().isAllow();
+            System.getProperty("user.dir");
+//            LOGGER.info("OPADecisionMaker Value of allow....{}", opaDecision);
+//            LOGGER.info("OPADecisionMaker my current Directory....{}", System.getProperty("user.dir"));
 
-            System.out.println("OPADecisionMaker Value of allow....");
-            System.out.print(opaDecision);
             return response.getResult().isAllow();
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
@@ -89,8 +134,9 @@ public class OPADecisionMaker {
         BulkOPAResponse response;
         try {
             String inputJson = objectMapper.writeValueAsString(opaRequest);
-            System.out.println("OPADecisionMaker BulkInputJSON created .....");
-            System.out.print(inputJson);
+
+          //  LOGGER.info("OPADecisionMaker BulkInputJSON created .....{}", inputJson);
+
             HttpRequest httpRequest = HttpRequest.newBuilder()
                     .POST(HttpRequest.BodyPublishers.ofString(inputJson))
                     .uri(URI.create(finegrainedendpoint))
@@ -100,8 +146,9 @@ public class OPADecisionMaker {
             HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             response = objectMapper.readValue(httpResponse.body(), BulkOPAResponse.class);
             allowedUris = response.getResult().getBatchAllow();
-            System.out.println("OPADecisionMaker Bulk Value of allow .....");
-            System.out.print(allowedUris);
+
+//            LOGGER.info("OPADecisionMaker Bulk Value of allow....{}", allowedUris);
+
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -113,108 +160,4 @@ public class OPADecisionMaker {
         }
     }
 
-    public static boolean isAllowedJarl(String uri, String method, List<String> roles) {
-        var file = new File(IRFile);
-        boolean allowed = false;
-
-        Map<String, ?> data = Map.of();
-        Map<String,Object> map = new HashMap<>();
-        map.put("roles", roles);
-        map.put("method","POST");
-        map.put("resource","files/upload/updateservice/package");
-
-        try {
-            System.out.println( "Allowed" );
-            var test1 = Jarl.builder(file)
-                    .build()
-                    .getPlan(entrypoint)
-                    .eval(map, data).getResults();
-            System.out.println( "test" );
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (allowed) {
-            System.out.println( "Allowed" );
-        }
-        return false;
-    }
-
-    public static List<String> isAllowedJarl(List < String > uris, String method, List < String > roles) {
-        var file = new File(IRFile2);
-        boolean allowed = false;
-   //     authz/redfish/v1/fine/policy/allow
-        Map<String, ?> data = Map.of();
-        Map<String,Object> map = new HashMap<>();
-
-        map.put("roles", Arrays.asList("OmcEquipmentObserver",
-                "CreateJob",
-                "DeleteJob",
-                "OmcEquipmentAdministrator"));
-        map.put("method","GET");
-
-        map.put("resources",Arrays.asList("/TaskService/Tasks/1/",
-                "files/upload/updateservice/package",
-                "/TaskService/Tasks/3/"));
-
-        try {
-            System.out.println( "Allowed" );
-            //authz/redfish/v1/fine/policy/allow
-            var test1 = Jarl.builder(file)
-                    .build()
-                    .getPlan(entrypoint2)
-                    .eval(map, data).getResults();
-
-            System.out.println( "test" );
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (allowed) {
-            System.out.println( "Allowed" );
-        }
-        return Collections.EMPTY_LIST;
-    }
-
-    public static List<String> isAllowed (List < String > uris, List < String >  methods, List < String > roles){
-        List<String> allowedUris = Collections.EMPTY_LIST;
-        ObjectMapper objectMapper = new ObjectMapper();
-        OPAInput1 input = new OPAInput1();
-        input.setMethods(methods);
-        input.setRoles(roles);
-        input.setResources(uris);
-        BulkOPARequest1 opaRequest = new BulkOPARequest1(input);
-
-        HttpClient httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
-        BulkOPAResponse response;
-        try {
-            String inputJson = objectMapper.writeValueAsString(opaRequest);
-            System.out.println("OPADecisionMaker BulkInputJSON created .....");
-            System.out.print(inputJson);
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .POST(HttpRequest.BodyPublishers.ofString(inputJson))
-                    .uri(URI.create(finegrainedendpoint))
-                    .setHeader("User-Agent", "PRASHANNA")
-                    .build();
-
-            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            response = objectMapper.readValue(httpResponse.body(), BulkOPAResponse.class);
-            allowedUris = response.getResult().getBatchAllow();
-            System.out.println("OPADecisionMaker Bulk Value of allow .....");
-            System.out.print(allowedUris);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            return allowedUris;
-        }
-    }
-
-
-    }
+}
